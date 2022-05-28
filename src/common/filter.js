16d6b9fab95230649b233a5c6eecd75d883556d6
perisetlapavan <perisetlapavan@gmail.com>
2022-05-28 17:40:54 +0530

    Checkpoint 4

--- src/common/filter.js
+++ src/common/filter.js
@@ -1,31 +1,126 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Input from '@mui/material/Input';
+// import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+// import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Select from '@mui/material/Select';
+import { genres } from '../assets/genre';
+import { artists } from '../assets/artists';
+// import { render } from '@testing-library/react';
 
-const ariaLabel = { 'aria-label': 'description' };
 
-export default function Filter() {
+// const bull = (
+//   <Box
+//     component="span"
+//     sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+//   >
+//     •
+//   </Box>
+// );
+
+export default class Filter extends React.Component {
+  // const [Genre, setGenre] = React.useState('');
+  state={
+    Genre:'',
+    Artists:''
+  }
+
+   handleChange1 = (event) => {
+    // setGenre(event.target.value);
+    this.setState({Genre:event.target.value});
+    // console.log(event.target.value);
+  };
+
+  // const [Artists, setArtists] = React.useState('');
+
+   handleChange2 = (event) => {
+    // setArtists(event.target.value);
+    this.setState({Artists:event.target.value});
+    // console.log(event.target.value);
+  };
+
+  handleInputChange = event => {
+    this.props.onDataChange([]);
+  }
+
+  render(){
   return (
-    <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1 },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <div style={{border: '0.5px solid', padding :'20px' ,width:'250px', float: 'right' , boxShadow: '5px'}}>
-      <h3>FIND MOVIES BY:</h3><br />      
-      <Input placeholder="Movie Name" inputProps={ariaLabel} /><br /><br />
-      <Input placeholder="Genre" inputProps={ariaLabel} /><br /><br />
-      <Input placeholder="Artists" inputProps={ariaLabel} /><br /><br />
-      <h5>Release Date Start</h5>
-      <Input type="Date" inputProps={ariaLabel} /><br />
-      <h5>Release Date End</h5>
-      <Input type="Date" inputProps={ariaLabel} /><br /><br />
-      <Button variant="contained">Apply</Button>
-      </div>
-    </Box>
+    <Card sx={{ maxWidth: 240 }} style={{ float: 'right', padding: '20px', margin: '16px' }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} variant="h3" color="text.secondary" gutterBottom>
+          FIND MOVIES BY:
+        </Typography><br />
+
+        <FormControl>
+          <TextField id="standard-basic" label="Movie Name" variant="standard" />
+        </FormControl>
+        <br />
+        <br />
+
+        <FormControl fullWidth variant="standard" >
+          <InputLabel id="demo-simple-select-standard-label" >Genre</InputLabel><br />
+          <Select
+            labelId="demo-simple-select-standard-label"
+            id="demo-simple-select-standard"
+            value={this.Genre}
+            label="Genre"
+            onChange={this.handleChange1}
+          >
+            {genres.map((item) => (
+              <MenuItem value={item.id}>{item.name}</MenuItem>
+            ))}
+
+          </Select>
+        </FormControl>
+        <br />
+        <br />
+        <FormControl fullWidth variant="standard" >
+          <InputLabel id="demo-simple-select-standard-label" >Artists</InputLabel>
+          <Select
+            labelId="demo-simple-select-standard-label"
+            id="demo-simple-select-standard"
+            value={this.Artists}
+            label="Artists"
+            onChange={this.handleChange2}
+          >
+            {artists.map((item) => (
+              <MenuItem value={item.id}>{item.first_name}  {item.last_name}</MenuItem>
+            ))}
+
+          </Select>
+        </FormControl>
+        <br />
+        <br />
+        <br />
+        <FormControl>
+          <InputLabel shrink={true}>Release Date Start</InputLabel>
+          <br />
+          <TextField id="standard-basic" type="date" variant="standard" />
+        </FormControl>
+
+        <br />
+        <br />
+        <br />
+        <FormControl>
+          <InputLabel shrink={true}>Release Date End</InputLabel>
+          <br />
+          <TextField id="standard-basic" type="date" variant="standard" />
+        </FormControl>
+
+      </CardContent>
+
+      <CardActions>
+        {/* <Button variant="contained" onClick={this.props.filter}>Apply</Button> */}
+        <Button variant="contained" onClick={this.handleInputChange}>Apply</Button>
+      </CardActions>
+    </Card>
   );
+}
 }
--- src/common/titlebarImageList.js
+++ src/common/titlebarImageList.js
@@ -5,36 +5,38 @@ import ImageListItemBar from '@mui/material/ImageListItemBar';
 // import ListSubheader from '@mui/material/ListSubheader';
 import IconButton from '@mui/material/IconButton';
 // import InfoIcon from '@mui/icons-material/Info';
-import {moviesData} from './moviesData';
+import { moviesData } from '../assets/moviesData';
 
-export default function TitlebarImageList() {
-  return (
-    <ImageList sx={{ height: 250}} style={{width:'100%', overflowX: 'scroll' }}>
-      <ImageListItem key="Subheader" cols={6} >
-        {/* <ListSubheader component="div">December</ListSubheader> */}
-      </ImageListItem>
-      {moviesData.map((item) => (
-        <ImageListItem key={item.poster_url}>
-          <img 
-            src={`${item.poster_url}?w=248&fit=crop&auto=format`}
-            srcSet={`${item.poster_url}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            alt={item.title}
-            loading="lazy"
-          />
-          <ImageListItemBar
-            title={item.title}
-            subtitle={item.author}
-            actionIcon={
-              <IconButton
-                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                aria-label={`info about ${item.title}`}
-              >
-                {/* <InfoIcon /> */}
-              </IconButton>
-            }
-          />
+export default class TitlebarImageList extends React.Component {
+  render() {
+    return (
+      <ImageList sx={{ height: 250 }} style={{ width: '100%' }}>
+        <ImageListItem key="Subheader" cols={6} >
+          {/* <ListSubheader component="div">December</ListSubheader> */}
         </ImageListItem>
-      ))}
-    </ImageList>
-  );
+        {moviesData.map((item) => (
+          <ImageListItem key={item.poster_url}>
+            <img
+              src={`${item.poster_url}?w=248&fit=crop&auto=format`}
+              srcSet={`${item.poster_url}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              alt={item.title}
+              loading="lazy"
+            />
+            <ImageListItemBar
+              title={item.title}
+              subtitle={item.author}
+              actionIcon={
+                <IconButton
+                  sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                  aria-label={`info about ${item.title}`}
+                >
+                  {/* <InfoIcon /> */}
+                </IconButton>
+              }
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+    );
+  }
 }
